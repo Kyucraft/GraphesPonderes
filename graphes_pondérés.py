d2771e222df153d795dd8eb2e6811a41f62bd41b
SiafroD <74325090+SiafroD@users.noreply.github.com>
2021-01-13 10:12:48 +0100

    Création classe Graphe_pondere + méthodes add/supp
    Création de la classe Graphe_pondere
    print(g) renvoie une str du graphe
    ajouter_sommet fonctionnel
    supprimer_sommet à terminer

---
+++ graphes_pondérés.py
@@ -0,0 +1,11 @@
+from graphes_pondérés_class import *
+
+g = Graphe_pondere({
+    'a' : {'b' : 4, 'c' : 7},
+    'b' : {'a' : 4, 'c' : 3},
+    'c' : {'a' : 7, 'b' : 3}
+})
+g.ajouter_sommet('d',{'b' : 5, 'c' : 6})
+print(g)
+g.supprimer_sommet('d')
+print(g)
---
+++ graphes_pondérés_class.py
@@ -0,0 +1,20 @@
+class Graphe_pondere:
+    def __init__(self,vois = {}):
+        self.vois = vois
+
+    def __repr__(self)->dict:
+        nouveau_dict = {}
+        for i in list(self.vois.keys()):
+            nouveau_dict[i] = self.vois[i]
+        return(f"{nouveau_dict}")
+
+    def ajouter_sommet(self,sommet,liens):
+        self.vois[sommet] = liens
+        for i in list(liens.keys()):
+            self.vois[i][sommet] = liens[i]
+
+    def supprimer_sommet(self,sommet):
+        liens_sommet = self.vois[sommet]
+        del self.vois[sommet]
+        for i in list(self.vois.keys()):
+            del self.vois[i][sommet.index(self.vois[i])]
