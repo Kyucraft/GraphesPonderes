5903ce7056e64a175c4a0f0be34a73c5d1868425
Kyucraft <42reki@gmail.com>
2021-03-18 11:37:00 +0100

    comm add

--- prioryty_queue.py
+++ prioryty_queue.py
@@ -6,19 +6,33 @@ class PriorityQueue:
 
 
     def __init__(self, taille: int, fill: T, ord: str):
-        #ordre est soit croissant ('ASC'), soit décroissant ('DESC')
+        '''
+        ord = ordre
+        Soit ASC = ascendant
+        Soit DESC = descendant
+        '''
         assert ord in ['ASC', 'DESC']
         self.ord=ord
         self.fill = fill
     
     def est_vide(self)->bool:
+        '''
+        Savoir si la queue est vide
+        '''
         return len(self.fill) == 0
         
     def enfiler(self,val: Tuple[T,int])-> None:
-        #Kiklou
+        '''
+        rajoute a la fin de la queues le val
+        '''
         self.fill.append(val)   
 
     def defiler(self)->Tuple[T,int]:
+        '''
+        Sert a defiler la valeur la plus petite si la file est en ascendant
+        Sert a defiler la valeur la plus grande si la file est en descendant
+        En sachant que la valeur est a la place 0
+        '''
         assert not(self.est_vide())
         self._maj_prio()
         defil_result = self.fill[0]
@@ -26,6 +40,9 @@ class PriorityQueue:
         return (defil_result) 
 
     def maj_sommet(self,sommet,val):
+        '''
+        Sert a rajouter une nouvelle valeur dans la queue
+        '''
         for i in self.fill:
             v,x = self.fill[i]
             if v==sommet:
@@ -34,21 +51,11 @@ class PriorityQueue:
                 
         
     def _maj_prio(self):
-        #kliklou
+        '''
+        trie les elements de la queues en fonction de leurs poids selon l'ordre
+        Du plus petit au plus grand avec ASC et du plus grand au plus petit avec DESC
+        '''
         self.fill = sorted(self.fill)
         if self.ord == 'DESC':
             self.fill.reverse()
         #trie les elem enfilés en fonction de leur poids selon ord
-     
-
-f = PriorityQueue(10, [], 'ASC')
-print(f.est_vide())
-f.enfiler(('a', 5))
-f.enfiler(('b', 3))
-f.enfiler(('c', 1))
-
-
-f = PriorityQueue(10, [], 'DESC')
-f.enfiler(('a', 5))
-f.enfiler(('b', 3))
-f.enfiler(('c', 1))
